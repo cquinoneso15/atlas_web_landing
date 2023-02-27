b45830e52f65b35c83130f54ccde0fb3f3f7ff54
Héctor Ochoa Ortiz <cie.hochoa@gmail.com>
2023-02-27 09:46:39 +0100

    Improve translation, get i18n js into its own file

--- index.js
+++ index.js
@@ -310,12 +310,13 @@ info.update = function (props) {
     if (biv) {
         this._div.innerHTML += (props
             ? '<b>' + props.name + '</b><br /> Acc. ' + props.value_acc.toFixed(2) + ' % (' + props.hilo_acc + ') - Pop. ' + props.value_pop.toFixed(2) + ' % (' + props.hilo_pop + ')'
-            : 'Hover over a neighborhood');
+            : '<span i18n="hover"></span>');
     } else {
         this._div.innerHTML += (props
             ? '<b>' + props.name + '</b><br />' + props.value.toFixed(2) + ' %'
-            : 'Hover over a neighborhood');
+            : '<span i18n="hover"></span>');
     }
+    translatePage();
 };
 
 info.addTo(map);
@@ -531,48 +532,3 @@ function handleJsonAreas(data) {
 /**********************
  * END AJAX CALLBACKS *
  **********************/
-
-/********
- * I18N *
- ********/
-
-function translatePage() {
-    document
-        .querySelectorAll("[i18n]")
-        .forEach(translateElement);
-}
-
-function translateElement(element) {
-    const key = element.getAttribute("i18n");
-    element.innerText = i18next.t(key);
-}
-
-function bindLocaleSwitcher(initialValue) {
-    const switcher =
-        document.querySelector("[data-i18n-switcher]");
-    switcher.value = initialValue;
-    switcher.onchange = (e) => {
-        // Set the locale to the selected option[value]
-        i18next.changeLanguage(e.target.value);
-    };
-}
-
-i18next
-    .use(i18nextHttpBackend)
-    .use(i18nextBrowserLanguageDetector)
-    .init({
-        supportedLngs: ["en", "de", "es"],
-        fallbackLng: "en",
-        debug: true,
-        nonExplicitSupportedLngs: true,
-        backend: {
-            loadPath: 'i18n/{{lng}}.json'
-        }
-    }, function(err, t) {translatePage(); 
-        bindLocaleSwitcher(i18next.language);});
-
-i18next.on('languageChanged', () => {translatePage();});
-
-/************
- * END I18N *
- ************/
---
+++ src/i18n.js
@@ -0,0 +1,44 @@
+/********
+ * I18N *
+ ********/
+
+function translatePage() {
+    document
+        .querySelectorAll("[i18n]")
+        .forEach(translateElement);
+}
+
+function translateElement(element) {
+    const key = element.getAttribute("i18n");
+    element.innerText = i18next.t(key);
+}
+
+function bindLocaleSwitcher(initialValue) {
+    const switcher =
+        document.querySelector("[data-i18n-switcher]");
+    switcher.value = initialValue;
+    switcher.onchange = (e) => {
+        // Set the locale to the selected option[value]
+        i18next.changeLanguage(e.target.value);
+    };
+}
+
+i18next
+    .use(i18nextHttpBackend)
+    .use(i18nextBrowserLanguageDetector)
+    .init({
+        supportedLngs: ["en", "de", "es"],
+        fallbackLng: "en",
+        debug: true,
+        nonExplicitSupportedLngs: true,
+        backend: {
+            loadPath: 'i18n/{{lng}}.json'
+        }
+    }, function(err, t) {translatePage(); 
+        bindLocaleSwitcher(i18next.language);});
+
+i18next.on('languageChanged', () => {translatePage();});
+
+/************
+ * END I18N *
+ ************/
