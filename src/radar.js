a6952d2347d5938920dd1f67308ba41d0d41b8d4
Héctor Ochoa Ortiz <cie.hochoa@gmail.com>
2023-03-08 11:21:00 +0100

    Add radar plot

---
+++ src/radar.js
@@ -0,0 +1,37 @@
+/*******************************
+ * radar.js                    *
+ * Script file for Radar Plots *
+ * Author: Héctor Ochoa Ortiz  *
+ * Affil.: TUM SVP             *
+ *******************************/
+
+
+function amenitiesRadarPlot(e) {
+    var layer = e.target;
+    var properties = layer.feature.properties;
+
+    const config = {
+        type: 'radar',
+        data: {
+            labels: ["Health", "Education", "Food", "Sports", "Community centers"],
+            datasets: [{
+                label: properties.name,
+                data: [properties.value_h, properties.value_e, properties.value_f, properties.value_s, properties.value_cc],
+                fill: true,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgb(255, 99, 132)',
+                pointBackgroundColor: 'rgb(255, 99, 132)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgb(255, 99, 132)'
+            }]
+        },
+        options: {
+          elements: {
+            line: {
+              borderWidth: 3
+            }
+          }
+        },
+      };
+}
