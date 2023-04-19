ff976758775d795c1d146934fe979f37036b23b1
Héctor Ochoa Ortiz <cie.hochoa@gmail.com>
2023-04-19 12:07:11 +0200

    Improve bivariate legend

--- src/bivariate.js
+++ src/bivariate.js
@@ -70,7 +70,7 @@ function handleJsonBiv(data) {
 
     // Add legend
     generateLegend('<svg width="150" height="150" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">' +
-        '<defs><marker id="arrow" viewBox="0 0 10 10" refX="5" refY="5" markerWidth="5" markerHeight="5" orient="auto-start-reverse"><path d="M 0 0 L 10 5 L 0 10 z" /></marker></defs>' +
+        '<defs><marker id="arrow" viewBox="0 0 10 10" refX="5" refY="5" markerWidth="4" markerHeight="4" orient="auto-start-reverse"><path d="M 0 0 L 10 5 L 0 10 z" /></marker></defs>' +
         '<g transform="translate(15 30) rotate(-45)">' +
         '<title>Bivariate legend</title>' +
         '<rect height="18" width="18" y="2" x="0" stroke="#000" fill="' + getColorBiv(getNonCritical(biv_quart_pop), getCritical(biv_quart_X)) + '"/>' +
