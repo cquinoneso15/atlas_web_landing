d3785cb30b8bc02812a10e97971c69d425f6cb52
Héctor Ochoa Ortiz <cie.hochoa@gmail.com>
2023-05-01 12:12:57 +0200

    Improve comments

--- src/legend.js
+++ src/legend.js
@@ -6,8 +6,10 @@
  */
 function generateLegend(info, replace) {
     if (replace) {
+        // Remove the current legend
         if (legend) { legend.remove(); }
 
+        // Add the new legend
         legend = L.control({ position: 'bottomright' });
         legend.onAdd = function (map) {
             var div = L.DomUtil.create('div', 'info legend');
@@ -16,8 +18,8 @@ function generateLegend(info, replace) {
             + '<div class="arrow"></div>'
             + '<div class="legend-content"></div>';
 
-            div.querySelector('input').checked = false;
-            div.querySelector('.legend-content').innerHTML = info;
+            div.querySelector('input').checked = false; // Not minimized
+            div.querySelector('.legend-content').innerHTML = info; // Add the info
             return div;
         };
 
